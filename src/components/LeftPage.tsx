66510b17c9dcb8626cbb49837c49cedfa23d535b
Jason E. Church <elijahchurch@gmail.com>
2023-09-15 15:27:04 -0700

    Create Option List component. WIP: style UI

--- src/components/LeftPage.tsx
+++ src/components/LeftPage.tsx
@@ -1,7 +1,12 @@
+import OptionList from "./OptionList";
+
 const LeftPage = () => {
     return(
         <div className="page">
-            <h1>EventPage</h1>
+            <div id="storyText">
+                <p>This is going to be where the story goes! Lot's of text should go here but not too much. Just writing a lot right now too see how it fills out the page. Need to lock in elements at some point. I probably can afford to give it more space</p>
+            </div>
+            <OptionList/>
         </div>
     )
 }
---
+++ src/components/OptionList.tsx
@@ -0,0 +1,7 @@
+const OptionList = () => {
+    return (
+        <h1>Options!!!</h1>
+    )    
+}
+
+export default OptionList;
