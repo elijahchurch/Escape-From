f6466e1c7ececf317e1d7f97a3f2d05023aba220
Jason E. Church <elijahchurch@gmail.com>
2023-09-22 14:54:20 -0700

    Remove commented out logic

--- src/components/CharacterInfo.tsx
+++ src/components/CharacterInfo.tsx
@@ -4,10 +4,6 @@ import { ICharacter } from "../interfaces";
 interface ICharacterInfo {character: ICharacter | null}
 
 const CharacterInfo = (props: ICharacterInfo) => {
-    // let flavorText = "";
-    // if(props.character !== null) {
-    //     flavorText = props.character.flavor;
-    // }
     return (
         <div id="characterInfo">
             <div id="characterPictureDiv">
