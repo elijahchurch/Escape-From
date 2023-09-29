192d0bfcb83694c6f32fa6e02129c8e9fb913433
Jason E. Church <elijahchurch@gmail.com>
2023-09-29 15:08:38 -0700

    Create logic in OptionList to set a variable of availableOptions based on option.conditional

--- src/components/OptionList.tsx
+++ src/components/OptionList.tsx
@@ -1,14 +1,33 @@
 import Option from "./Option";
 import React from "react";
-import { IOption } from "../interfaces";
+import { IOption, IItem} from "../interfaces";
+import {useContext} from "react";
+import { charaContext } from "../context/charaContext";
 
 interface IOptionList { options: IOption[], findPage: (id:string) => void}
 
 const OptionList = (props : IOptionList) => {
+    
+    const gameCharacter = useContext(charaContext);
+
+    let availableOptions :IOption[] = [];
+    props.options.forEach((option: IOption) => {
+        if(option.conditional === true) {
+            availableOptions.push(option)
+        }
+        else {
+            gameCharacter.inventory.forEach((item: IItem) => {
+                if(item.id === option.itemRef) {
+                    availableOptions.push(option)
+                }
+            })
+        }
+    });
 
     return (
         <div>
-            {props.options.map((element, index) => (
+            
+            {availableOptions.map((element, index) => (
                 <Option 
                 option ={element}
                 findPage={props.findPage}
