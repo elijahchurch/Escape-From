2ea51f242b1b9ecfd9aa72c21ab0411b1c771912
Jason E. Church <elijahchurch@gmail.com>
2023-10-04 16:20:32 -0700

    Create logo/background and style components

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -2,15 +2,14 @@ import {Link} from "react-router-dom"
 
 const Start = () => {
     return(
-        <div className="initialcontainers">
-            <h1>Hi!!</h1>
-
-            <button className="gameButton">
+        <div id="logo">
+            <button className="gameButton" id="start">
                     <span className="transition"></span>
                     <span className="gradient"></span>
                     <Link className="label linkstyles" to="/game">Start Game</Link>
                 </button>
         </div>
+        
     )
 }
 
